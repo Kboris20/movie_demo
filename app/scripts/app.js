9b16b0aeeadc92f4056359e78af7240baf0d333a
Kboris20 <klettboris@gmail.com>
2015-06-25 21:15:32 +0200

    vers1

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name movieDemoApp
+ * @description
+ * # movieDemoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('movieDemoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name movieDemoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the movieDemoApp
+ */
+angular.module('movieDemoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+	
+	$scope.monClick = function(){
+		$scope.awesomeThings.push("Boris Klett");
+	}
+  }
+  
+  );
