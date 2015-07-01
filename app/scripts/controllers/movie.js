6d8ffc74435bc9697b101782cac3a6449cb26513
Kboris20 <klettboris@gmail.com>
2015-07-01 11:11:23 +0200

    vers7

--- app/scripts/controllers/movie.js
+++ app/scripts/controllers/movie.js
@@ -13,7 +13,7 @@ angular.module('movieDemoApp')
   $rootScope.moviesdb = moviesdb
    var id = $routeParams.id;
 
-     $http.get('http://amc.ig.he-arc.ch:3003/movie/'+id+'?append_to_response=similar,releases,credits')
+     $http.get('http://amc.ig.he-arc.ch:3003/movie/'+id+'?append_to_response=similar,releases,credits&language=fr')
       .success(function(data){
         $scope.film = data;
         $rootScope.bgImage = $scope.film.backdrop_path;
